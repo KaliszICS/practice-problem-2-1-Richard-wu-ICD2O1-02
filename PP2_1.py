0bbcd91952028b267e3b34f741ba4df6e41eba69
Richard-wu-ICD2O1-02 <Richard.wu4@student.tdsb.on.ca>
2024-11-05 14:13:18 -0500

    Update PP2_1.py

--- PP2_1.py
+++ PP2_1.py
@@ -2,11 +2,28 @@
 
 def q1(): 
   #Write Assignment code here
-  
+
+  num = int(input("Input an integer: "))
+
+#if <condition>:
+if num < 10:
+    #Code that runs when the if statement is true is indent in once
+    #This code is skipped when the condition is false
+    print("The number you provided was less than 10")
+    num = num + 9
 
 
 def q2(): 
   #Write Assignment code here
+
+  num = int(input("Input an integer: "))
+
+#if <condition>:
+if num > 10:
+    #Code that runs when the if statement is true is indent in once
+    #This code is skipped when the condition is false
+    print("The number you provided was greater than 10")
+    num = num - 9
 
 
 
